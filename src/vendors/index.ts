29819b3132bb8771e184a45bb47f6176b678cf1d
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-06-22 11:10:57 +0530

    chore: minor changes

--- src/vendors/index.ts
+++ src/vendors/index.ts
@@ -4,21 +4,22 @@ import { errorMessageWrapper, type ToJsonSchemaFn } from "./utils.js";
 import getValibotToJsonSchemaFn from "./valibot.js";
 import getZodToJsonSchemaFn from "./zod.js";
 
+const vendorToFn: Record<string, () => Promise<ToJsonSchemaFn>> = {
+  arktype: getArktypeToJsonSchemaFn,
+  effect: getEffectToJsonSchemaFn,
+  valibot: getValibotToJsonSchemaFn,
+  zod: getZodToJsonSchemaFn,
+};
+
 export const getToJsonSchemaFn = async (
   vendor: string,
 ): Promise<ToJsonSchemaFn> => {
-  switch (vendor) {
-    case "arktype":
-      return getArktypeToJsonSchemaFn();
-    case "effect":
-      return getEffectToJsonSchemaFn();
-    case "valibot":
-      return getValibotToJsonSchemaFn();
-    case "zod":
-      return getZodToJsonSchemaFn();
-    default:
-      throw new Error(
-        errorMessageWrapper(`Unsupported schema vendor "${vendor}"`),
-      );
-  }
+  const venderConvertor = vendorToFn[vendor];
+
+  if (!venderConvertor)
+    throw new Error(
+      errorMessageWrapper(`Unsupported schema vendor "${vendor}"`),
+    );
+
+  return venderConvertor();
 };
