3142c1e8892cba4b7b0f95bd78002d5236d8f4d4
kingbri <8082010+kingbri1@users.noreply.github.com>
2025-06-28 11:48:14 -0400

    Zod: Add override to flatten schemas
    Properly flattens out transforms and edge cases that can cause runtime
    errors.
    
    Signed-off-by: kingbri <8082010+kingbri1@users.noreply.github.com>

--- src/vendors/zod.ts
+++ src/vendors/zod.ts
@@ -8,9 +8,31 @@ export default async function getToJsonSchemaFn(): Promise<ToJsonSchemaFn> {
 
     // https://zod.dev/library-authors?id=how-to-support-zod-and-zod-mini-simultaneously#how-to-support-zod-3-and-zod-4-simultaneously
     if ("_zod" in (schema as $ZodType | ZodTypeAny)) {
-      handler = await tryImport(import("zod/v4/core"), "zod v4").then(
-        (mod) => mod.toJSONSchema as ToJsonSchemaFn,
-      );
+      const toJSONSchema = await tryImport(
+        import("zod/v4/core"),
+        "zod v4",
+      ).then((mod) => mod.toJSONSchema);
+
+      // Add flatten override function
+      // TODO (kingbri): Unsure how the override should be handled
+      // So for now, use this as the default unless the user specifies otherwise
+      const createJsonSchema = (
+        schema: $ZodType,
+        options?: Record<string, unknown>,
+      ) =>
+        toJSONSchema(schema, {
+          override: (ctx) => {
+            // Flattens nested allOfs with intersections
+            if (ctx.jsonSchema.allOf) {
+              ctx.jsonSchema.allOf = ctx.jsonSchema.allOf.flatMap((schema) =>
+                schema.allOf ? schema.allOf : [schema],
+              );
+            }
+          },
+          ...(options ?? {}),
+        });
+
+      handler = createJsonSchema as ToJsonSchemaFn;
     } else {
       handler = await tryImport(import("zod-to-json-schema"), "zod v3").then(
         (mod) => mod.zodToJsonSchema as ToJsonSchemaFn,
